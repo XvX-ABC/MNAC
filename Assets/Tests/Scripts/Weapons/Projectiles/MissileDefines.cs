141f7d6e8aefe6d7392c77c3d95817c319616b4e
yanhaoran <yanhaoran_xvx@163.com>
2025-01-15 22:22:50 +0800

    20250115

---
+++ Assets/Tests/Scripts/Weapons/Projectiles/MissileDefines.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Tests.Weapons.Projectiles
+{
+    public class MissileDefines : MonoBehaviour, IMissileDefines
+    {
+        [SerializeField]
+        float _angularSpeed;
+        [SerializeField]
+        float _maxAngle;
+        [SerializeField]
+        float _speed;
+
+        public float AngularSpeed { get => _angularSpeed; }
+        public float MaxAngle { get => _maxAngle; }
+        public float Speed { get => _speed; }
+    }
+}
