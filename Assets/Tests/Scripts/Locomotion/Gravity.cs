15d81bc1d020a8b3951e3ba3d7a6ef32eb681c4a
yanhaoran <yanhaoran_xvx@163.com>
2025-01-15 22:10:28 +0800

    Revert "perf: 简化重力计算方式及受重力影响的部分"
    This reverts commit e67fbe65629433e5840d5ac6188120eddf6f0deb.

--- Assets/Tests/Scripts/Locomotion/Gravity.cs
+++ Assets/Tests/Scripts/Locomotion/Gravity.cs
@@ -4,36 +4,20 @@ namespace Tests.Locomotion
 {
     public class Gravity : IModule
     {
-        float _time;
-        float _v;
+
         public Gravity()
         {
         }
 
         public void OnUpdate(Context context)
         {
-            //if (context.State == State.Descending)
-            //{
-            //    Debug.Log("Gravit working");
-            //    var currentVelocity = context.Velocity;
-
-            //    currentVelocity.y += Physics.gravity.y * context.DeltaTime;
-            //    context.Velocity = currentVelocity;
-            //}
-
             if (context.State == State.Descending)
             {
-                var velocity = context.Velocity;
-                velocity.y = Physics.gravity.y * _time;
+                var currentVelocity = context.Velocity;
 
-
-                context.Velocity = velocity;
-
-
-                _time += context.DeltaTime;
+                currentVelocity.y += Physics.gravity.y * context.DeltaTime;
+                context.Velocity = currentVelocity;
             }
-            else if (_time > 0)
-                _time = 0;
         }
     }
 }
--- Assets/Tests/Scripts/Locomotion/HorizontalDrag.cs
+++ Assets/Tests/Scripts/Locomotion/HorizontalDrag.cs
@@ -14,16 +14,16 @@ namespace Tests.Locomotion
 
         Vector3 CalculateVelocityWithDrag(Vector3 velocity, float deltaTime)
         {
+
             var result = velocity * (1 - deltaTime * _defines.Drag);
+            result.y = velocity.y;
             return result;
-
-
         }
         public void OnUpdate(Context context)
         {
             var direction = context.Input.HorizontalDirection;
             if (direction == Vector3.zero)
-                context.Velocity = CalculateVelocityWithDrag(context.Velocity, context.DeltaTime);
+                context.Velocity = CalculateVelocityWithDrag(context.Velocity, _defines.Drag);
         }
     }
 }
