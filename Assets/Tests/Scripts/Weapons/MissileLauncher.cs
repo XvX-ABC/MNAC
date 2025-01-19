56026b084ebdd73714fd14cd6590e23d627cca42
yanhaoran <yanhaoran_xvx@163.com>
2025-01-19 22:26:02 +0800

    feat: 实现简单的单仓和多仓飞弹发射器，还有部分功能需要完善

---
+++ Assets/Tests/Scripts/Weapons/MissileLauncher.cs
@@ -0,0 +1,75 @@
+﻿using Assets.Scripts.Arms.Actions;
+using Assets.Scripts.Utilities.Timeline;
+using Assets.Scripts.Utilities.Timeline.Event.Point;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests;
+using Tests.Weapons;
+using Tests.Weapons.Projectiles;
+using Unity.XR.OpenVR;
+using UnityEngine;
+using UnityEngine.SearchService;
+
+namespace Assets.Tests.Scripts.Weapons
+{
+    public class MissileLauncher : LauncherBase
+    {
+        protected GameObject missileObj;
+        public ITarget Target;
+        protected override void Awake()
+        {
+            base.Awake();
+            if (!defines.AmmoOrigin.TryGetComponent<IMissile>(out _))
+                throw new ComponentCantFoundException(defines.AmmoOrigin, typeof(IMissile));
+
+        }
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            Reload();
+        }
+        protected override void GetAmmo(GameObject obj)
+        {
+            obj.transform.localPosition = defines.MagazinePosition;
+            obj.transform.localRotation = Quaternion.identity;
+            obj.SetActive(true);
+            var missile = obj.GetComponent<IMissile>();
+            missile.Enabled = false;
+        }
+        protected override Timeline CreateReloadTimeline()
+        {
+            var timeline = new Timeline(defines.ReloadDuration);
+            timeline.AddPointEvent(0, _ => Reload());
+            return timeline;
+        }
+        internal override void Reload()
+        {
+            base.Reload();
+            if (missileObj == null)
+                missileObj = ammoPool.Get();
+        }
+
+        public override void Launch()
+        {
+            if (!enabled || reloadTimeline.isRunning)
+                return;
+
+            if (Time.time - lastLaunchTime <= launchingInterval || ammoQuantityInMagazine <= 0)
+                return;
+
+            missileObj.transform.SetParent(null);
+            var missile = missileObj.GetComponent<IMissile>();
+
+            //missile.Target = Target;
+            missile.Enabled = true;
+
+            missileObj = null;
+
+            ammoQuantityInMagazine--;
+            lastLaunchTime = Time.time;
+        }
+    }
+}
---
+++ Assets/Tests/Scripts/Weapons/MultiLauncher.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tests.Weapons;
+using UnityEngine;
+
+namespace Assets.Tests.Scripts.Weapons
+{
+    public class MultiLauncher : MonoBehaviour, ILauncher
+    {
+        ILauncher[] _subLaunchers;
+        [SerializeField]
+        ushort _ammoSpareQuantity;
+        [SerializeField]
+        ushort _ammoQuantityInMagazine;
+        ILauncherDefines _defines;
+        Action<ILauncher> _initializationAction;
+        public ushort SpareCount { get => _ammoSpareQuantity; }
+        public ushort MagazineCount { get => _ammoQuantityInMagazine; }
+        public ILauncherDefines Defines { get => _defines; }
+        public Action<ILauncher> InitializationAction { get => _initializationAction; set => _initializationAction = value; }
+
+        void Awake()
+        {
+            var list = GetComponentsInChildren<ILauncher>().ToList();
+            if (list.Contains(this))
+                list.Remove(this);
+            _subLaunchers = list.ToArray();
+            _defines = GetComponent<ILauncherDefines>() ?? throw new ComponentCantFoundException(this.gameObject, typeof(ILauncherDefines));
+
+            foreach (var l in _subLaunchers)
+                l.InitializationAction += launcher => launcher.Supply(-(launcher.Defines.AmmoSpareQuantity - 1));
+        }
+        void Start()
+        {
+            _ammoSpareQuantity = (ushort)(_defines.AmmoTotalQuantity - _subLaunchers.Length);
+            _initializationAction?.Invoke(this);
+        }
+        void Update()
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+                Launch();
+            if (Input.GetKeyDown(KeyCode.R))
+                StartReload();
+            if (Input.GetKeyDown(KeyCode.Space))
+                Supply(10);
+        }
+        public void Launch()
+        {
+            if (!enabled)
+                return;
+            foreach (var l in _subLaunchers)
+                l.Launch();
+            _ammoQuantityInMagazine -= (ushort)_subLaunchers.Length;
+        }
+
+        public bool StartReload()
+        {
+            if (!enabled)
+                return false;
+            foreach (var l in _subLaunchers)
+            {
+                if (_ammoSpareQuantity <= 0)
+                    return false;
+
+                l.Supply(1);
+                _ammoSpareQuantity--;
+                if (!l.StartReload())
+                {
+                    l.Supply(-1);
+                    _ammoSpareQuantity++;
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool EndReload()
+        {
+            if (!enabled)
+                return false;
+            foreach (var l in _subLaunchers)
+            {
+                if (!l.EndReload())
+                {
+                    l.Supply(-1);
+                    _ammoSpareQuantity++;
+                    return false;
+                }
+                _ammoQuantityInMagazine++;
+            }
+            return true;
+        }
+
+        public int Supply(int num)
+        {
+            if (_ammoSpareQuantity + num < 0)
+                return 0;
+            var suppNum = Mathf.Min(_defines.AmmoTotalQuantity - _subLaunchers.Length - _ammoSpareQuantity, num);
+            _ammoSpareQuantity += (ushort)suppNum;
+            return suppNum;
+        }
+    }
+}
