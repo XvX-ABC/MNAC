031045515aee775712de27a0676009a3f4a9a358
yanhaoran <yanhaoran_xvx@163.com>
2025-01-19 22:11:31 +0800

    refactor: 删除不必要的字段和属性

--- Assets/Tests/Scripts/Weapons/Projectiles/MissileDefines.cs
+++ Assets/Tests/Scripts/Weapons/Projectiles/MissileDefines.cs
@@ -10,13 +10,10 @@ namespace Tests.Weapons.Projectiles
         float _maxAngle;
         [SerializeField]
         float _speed;
-        [SerializeField]
-        float _angularAngle;
 
         public float AngularSpeed { get => _angularSpeed; }
         public float MaxAngle { get => _maxAngle; }
         public float Speed { get => _speed; }
 
-        public float AngularAngle { get => _angularAngle; }
     }
 }
