b50233a9a2548c7b5ae324afa8725c1b637dee21
yanhaoran <yanhaoran_xvx@163.com>
2025-01-21 22:48:31 +0800

    feat: 新添加ITimeline接口

---
+++ Assets/Scripts/Utilities/Timeline/ITimeline.cs
@@ -0,0 +1,18 @@
+﻿using Assets.Scripts.Utilities.Timeline.Event;
+
+namespace Assets.Scripts.Utilities.Timeline
+{
+    public interface ITimeline
+    {
+        bool IsRunning { get; }
+        float Time { get; }
+
+        void Continue();
+        void OnUpdate(float deltaTime);
+        void Start();
+        void Stop();
+        string ToString();
+        bool AddEvent(ITimelineEvent evt);
+        bool RemoveEvent(ITimelineEvent evt);
+    }
+}
