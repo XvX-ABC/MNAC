a3163295138511e974e4ae6df9c4c563d330dd4f
yanhaoran <yanhaoran_xvx@163.com>
2025-01-21 22:49:34 +0800

    feat: 新增行为锁功能类，为对象方法的锁定提供简单支持

---
+++ Assets/Scripts/Utilities/ActionsLock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace Utilities
+{
+    public class ActionsLock<T> where T : Enum
+    {
+        byte _lockArray;
+        Type _enumType;
+        public ActionsLock(bool defaultValue = false)
+        {
+            _enumType = typeof(T);
+            var length = Enum.GetNames(_enumType).Length;
+            if (length >= 8)
+                throw new Exception();
+            _lockArray = defaultValue ? byte.MaxValue : (byte)0;
+        }
+        public bool IsLocked(T e)
+        {
+            var v = Convert.ToByte(e);
+            return (_lockArray & Convert.ToByte(e)) == v;
+        }
+        public void Lock(params T[] es)
+        {
+            foreach (var e in es)
+                _lockArray |= Convert.ToByte(e);
+        }
+        public void Unlock(params T[] es)
+        {
+            foreach (var e in es)
+            {
+                var v = Convert.ToByte(e);
+                if ((_lockArray & v) == v)
+                    _lockArray ^= v;
+            }
+        }
+
+        public void LockAll()
+        {
+            _lockArray = byte.MaxValue;
+        }
+        public void UnlockAll()
+        {
+            _lockArray = 0;
+        }
+    }
+}
---
+++ Assets/Tests/Scripts/Weapons/LauncherActionsLock.cs
@@ -0,0 +1,20 @@
+﻿using Utilities;
+using ActionsEnum = Tests.Weapons.ILauncher.ActionsEnum;
+namespace Tests.Weapons
+{
+    public class LauncherActionsLock : ActionsLock<ActionsEnum>
+    {
+        public bool LaunchIsLocked()
+        {
+            return IsLocked(ActionsEnum.Launch);
+        }
+        public bool StartReloadIsLocked()
+        {
+            return IsLocked(ActionsEnum.StartReload);
+        }
+        public bool EndReloadIsLocked()
+        {
+            return !IsLocked(ActionsEnum.EndReload);
+        }
+    }
+}
