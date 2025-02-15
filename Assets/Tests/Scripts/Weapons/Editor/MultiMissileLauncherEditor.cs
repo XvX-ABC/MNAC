30cd4342b13d2fbc70b272f9e14c87e0099f39e6
yanhaoran <yanhaoran_xvx@163.com>
2025-02-15 22:56:19 +0800

    20250215

---
+++ Assets/Tests/Scripts/Weapons/Editor/MultiMissileLauncherEditor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Weapons;
+using UnityEditor;
+
+namespace Assets.Tests.Scripts.Weapons.Editor
+{
+    public class MultiMissileLauncherEditor : UnityEditor.Editor
+    {
+        IMissileLauncher[] _subLaunchers;
+        private void OnEnable()
+        {
+            var i = target as MultiMissileLauncher;
+            _subLaunchers = (IMissileLauncher[])typeof(MultiMissileLauncher).GetField("subLaunchers", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(i);
+        }
+        void ShowDefinesOfSubLaunchers()
+        {
+            foreach(var l in _subLaunchers)
+            {
+                var defines = l.Defines;
+
+            }
+        }
+        public override void OnInspectorGUI()
+        {
+
+        }
+    }
+}
