058bdcf9c2fe2d3c02906136816d95dbdb8666fd
yanhaoran <yanhaoran_xvx@163.com>
2025-11-12 11:54:10 +0800

    refacotr：17176f25的补充提交

---
+++ Assets/Tests/Scripts/Characters/Character_Debug/Character_Debug.cs
@@ -0,0 +1,10 @@
+﻿using Tests.Characters.Interaction;
+using Tests.Interaction;
+
+namespace Tests.Characters
+{
+    [Interactable]
+    public class Character_Debug : CharacterBase
+    {
+    }
+}
