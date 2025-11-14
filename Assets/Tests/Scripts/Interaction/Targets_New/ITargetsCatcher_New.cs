46899a029d558aa9fe3e49bacb3dcab09005bfc4
yanhaoran <yanhaoran_xvx@163.com>
2025-11-14 10:07:35 +0800

    refactor：迁移目录

---
+++ Assets/Tests/Scripts/Interaction/Targets_New/ITargetsCatcher_New.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tests.Interaction
+
+{
+    public interface ITargetsCatcher_New<T> : ICatcher<T> where T : ITarget_New
+    {
+    }
+}
