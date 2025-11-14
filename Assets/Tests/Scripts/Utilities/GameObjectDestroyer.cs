c4c3501f58f2e830579c54795ed49af303437316
yanhaoran <yanhaoran_xvx@163.com>
2025-11-14 09:52:56 +0800

    feat：增加Destroyer用于调试

---
+++ Assets/Tests/Scripts/Utilities/GameObjectDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tests.Utilities
+{
+    internal class GameObjectDestroyer : MonoBehaviour
+    {
+        [SerializeField]
+        KeyCode _keyCode;
+        [SerializeField]
+        GameObject _obj;
+
+        public GameObject Obj { get => _obj; set => _obj = value; }
+        public KeyCode KeyCode { get => _keyCode; set => _keyCode = value; }
+
+        private void Update()
+        {
+            if (UnityEngine.Input.GetKeyDown(_keyCode) && _obj != null)
+                GameObject.Destroy(_obj);
+        }
+    }
+}
