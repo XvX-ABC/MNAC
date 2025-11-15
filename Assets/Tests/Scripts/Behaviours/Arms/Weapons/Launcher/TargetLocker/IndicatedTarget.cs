560b193f08571dc2d536eb494024f6303e7266d9
yanhaoran <yanhaoran_xvx@163.com>
2025-11-15 16:50:39 +0800

    refactor：迁移目录

--- Assets/Tests/Scripts/Behaviours/Arms/Weapons/Launcher/TargetLocker/IndicatedTarget.cs
+++ Assets/Tests/Scripts/Behaviours/Arms/Weapons/Launcher/TargetLocker/IndicatedTarget.cs
@@ -4,7 +4,6 @@ namespace Tests.Behaviours.Arms.Weapons.Launcher
 {
     internal class IndicatedTarget : UI.IndicatedTarget
     {
-        public override Vector3 Position => transform.position;
         protected override void OnEnable()
         {
             indicatorType = UI.IndicatorType.Box;
