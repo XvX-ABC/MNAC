5fa2df625452600daf942b48393ce4b0621560b3
yanhaoran <yanhaoran_xvx@163.com>
2025-11-20 20:10:56 +0800

    feat：新增IWeapon接口

---
+++ Assets/Tests/Scripts/Weapons_New/IWeapon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Tests.Weapons_New
+{
+    public interface IWeapon
+    {
+        Guid ID { get; }
+        string Name { get; }
+        WeaponType Type { get; }
+    }
+}
