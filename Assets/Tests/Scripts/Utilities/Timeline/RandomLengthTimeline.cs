b340a9e93383ed2af570bdbb64a27d341f9a6205
yanhaoran <yanhaoran_xvx@163.com>
2025-11-20 19:56:15 +0800

    feat：新增RandomTimeline_V1，作为临时使用

--- Assets/Tests/Scripts/Utilities/Timeline/RandomLengthTimeline.cs
+++ Assets/Tests/Scripts/Utilities/Timeline/RandomLengthTimeline.cs
@@ -3,6 +3,33 @@ using UnityEngine;
 
 namespace Tests.Utilities.Timeline
 {
+    public class RandomLengthTimeline_V1 : Timeline_V1
+    {
+        internal ITimeGenerator timeGenerator;
+        public RandomLengthTimeline_V1(ITimeGenerator timeGeneration) : base(0)
+        {
+            timeGenerator = timeGeneration;
+        }
+        public RandomLengthTimeline_V1(Vector2 range) : this(new RandomTimeGeneration(range))
+        {
+
+        }
+        public override void Restart()
+        {
+            if (timeGenerator != null)
+                length = timeGenerator.Time;
+            base.Restart();
+        }
+        public override void Reset()
+        {
+            base.Reset();
+            time = 0;
+        }
+        public override bool UpdateLength(float newLength)
+        {
+            return true;
+        }
+    }
     public class RandomLengthTimeline : Timeline
     {
         internal ITimeGenerator timeGenerator;
