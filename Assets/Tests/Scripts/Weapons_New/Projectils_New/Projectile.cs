56f1efb51284dc1fa4a69bce336e86fa2197790d
yanhaoran <yanhaoran_xvx@163.com>
2025-11-21 19:13:15 +0800

    feat：新增子组件和相关逻辑

--- Assets/Tests/Scripts/Weapons_New/Projectils_New/Projectile.cs
+++ Assets/Tests/Scripts/Weapons_New/Projectils_New/Projectile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tests.Utilities.Blackboards;
 using Tests.Weapons_New.Projectiles;
 using UnityEngine;
 
@@ -6,15 +7,31 @@ namespace Tests.Weapons.Projectiles_New
 {
     public abstract class Projectile : MonoBehaviour, IProjectile
     {
+        [SerializeField]
+        ProjectileComponent[] _subComponents;
         Action<IProjectile> _actionStartCallback;
         Action<IProjectile> _actionEndCallback;
+        protected Blackboard blackboard;
 
         public GameObject Obj => this.gameObject;
 
         public Action<IProjectile> ActionStartCallback { get => _actionStartCallback; set => _actionStartCallback = value; }
         public Action<IProjectile> ActionEndCallback { get => _actionEndCallback; set => _actionEndCallback = value; }
         public abstract Action<IProjectile, GameObject> HitAction { get; set; }
-
+        protected virtual void Awake()
+        {
+            blackboard = new();
+        }
+        protected virtual void OnEnable()
+        {
+            foreach (var comp in _subComponents)
+                comp.Initialize(blackboard);
+        }
+        protected virtual void OnDisable()
+        {
+            foreach (var comp in _subComponents)
+                comp.Dispose();
+        }
         public virtual void EndAction()
         {
             this.enabled = false;
