31ab085b599942531c065d287d34d37fcfc71159
yanhaoran <yanhaoran_xvx@163.com>
2025-11-21 19:07:20 +0800

    feat：ICatcher接口新增属性

--- Assets/Tests/Scripts/Interaction/Targets_New/ICatcher.cs
+++ Assets/Tests/Scripts/Interaction/Targets_New/ICatcher.cs
@@ -9,6 +9,9 @@ namespace Tests.Interaction
         public bool Enabled { get; set; }
         public IReadOnlyList<T> CaughtItems { get; }
         public Action<List<T>> CaughtItemsChangedAction { get; set; }
+        Action<T> ItemCaughtAction { get; set; }
+        Action<T> ItemReleaseAction { get; set; }
+
         public void AddItem(T target);
         public void RemoveItem(T target);
     }
