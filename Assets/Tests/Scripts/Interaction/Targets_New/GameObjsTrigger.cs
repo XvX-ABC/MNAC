a76f867dd9719f83b7503e37e22d978edb5b69a2
yanhaoran <yanhaoran_xvx@163.com>
2025-11-21 19:06:41 +0800

    refactor：新增目标触发器

---
+++ Assets/Tests/Scripts/Interaction/Targets_New/GameObjsTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Tests.Interaction
+{
+    public class GameObjsTrigger : TargetsTriggerBase<GameObject>
+    {
+        public GameObjsTrigger(Collider collider) : base(collider)
+        {
+        }
+
+        public override void OnTriggerEnter(Collider collider)
+        {
+            var obj = collider.gameObject;
+            AddItemImpl(obj);
+        }
+
+        public override void OnTriggerExit(Collider collider)
+        {
+            var obj = collider.gameObject;
+            RemoveItemImpl(obj);
+        }
+    }
+}
---
+++ Assets/Tests/Scripts/Interaction/Targets_New/TargetsTriggerBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Tests.Interaction
+{
+    public abstract class TargetsTriggerBase<T> : CatcherBase<T>
+    {
+        [SerializeField]
+        Collider _collider;
+        public LayerMask IncludeMask { get => _collider.includeLayers; set => _collider.includeLayers = value; }
+        public LayerMask ExcludeMask { get => _collider.excludeLayers; set => _collider.excludeLayers = value; }
+        public TargetsTriggerBase(Collider collider)
+        {
+            _collider = collider ?? throw new ArgumentNullException(nameof(collider));
+        }
+        public abstract void OnTriggerEnter(Collider collider);
+        public abstract void OnTriggerExit(Collider collider);
+    }
+}
