accb440065f528abdea2c7839b42cfa4e5bb8d0f
yanhaoran <yanhaoran_xvx@163.com>
2025-12-01 12:24:27 +0800

    refactor：重命名为EnvironmentCore

--- Assets/Tests/Scripts/Characters/EnvironmentCore.cs
+++ Assets/Tests/Scripts/Characters/EnvironmentCore.cs
@@ -1,4 +1,5 @@
-﻿using Tests.TPhysics;
+﻿using Tests.Characters.Humanoid;
+using Tests.TPhysics;
 using Tests.TPhysics.Environment;
 using Tests.Utilities.Blackboards;
 using Tests.Utilities.Composable;
@@ -6,19 +7,24 @@ using UnityEngine;
 using Core = Tests.TPhysics.Environment.EnvironmentCore;
 namespace Tests.Characters
 {
-    [RequireComponent(typeof(Rigidbody))]
-    public class EnvironmentCore_MonoComponent : ComponentBase_MonoComponent
+    [RequireComponent(typeof(CapsuleCollider))]
+    internal class EnvironmentCore : HumanoidCollisionComponent
     {
         Rigidbody _rbody;
         Core _core;
+        CapsuleCollider _collider;
         internal GroundDetector groundDetector => _core.GroundDetector;
         internal World world => _core.World;
+
+        public Rigidbody Rbody { get => _rbody; set => _rbody = value; }
+        public CapsuleCollider Collider { get => _collider; set => _collider = value; }
+
         protected override void Awake()
         {
             base.Awake();
+            _collider = GetComponent<CapsuleCollider>();
             var definitions = GetComponent<IEnvironmentDefinitions>() ?? throw new ComponentCantFindException(gameObject, typeof(IEnvironmentDefinitions));
             _core = new Core(definitions);
-            _rbody = GetComponent<Rigidbody>();
         }
         void OnEnable()
         {
@@ -28,24 +34,40 @@ namespace Tests.Characters
         {
             groundDetector.Enabled = false;
         }
-        private void OnCollisionEnter(Collision collision)
+        //private void OnCollisionEnter(Collision collision)
+        //{
+
+        //    groundDetector.OnCollisionEnter(collision);
+        //}
+
+        //private void OnCollisionExit(Collision collision)
+        //{
+        //    groundDetector.OnCollisionExit(collision);
+        //}
+
+        //private void OnCollisionStay(Collision collision)
+        //{
+        //    groundDetector.OnCollisionStay(collision);
+        //}
+        protected internal override void OnCollisionEnterImpl(Collision collision)
         {
+            base.OnCollisionEnterImpl(collision);
             groundDetector.OnCollisionEnter(collision);
         }
-
-        private void OnCollisionExit(Collision collision)
+        protected internal override void OnCollisionExitImpl(Collision collision)
         {
+            base.OnCollisionExitImpl(collision);
             groundDetector.OnCollisionExit(collision);
         }
-
-        private void OnCollisionStay(Collision collision)
+        protected internal override void OnCollisionStayImpl(Collision collision)
         {
+            base.OnCollisionStayImpl(collision);
             groundDetector.OnCollisionStay(collision);
         }
         private void FixedUpdate()
         {
 
-            groundDetector.Position = _rbody.position;
+            groundDetector.Position = _rbody?.position ?? this.transform.position;
             groundDetector.OnFixedUpdate();
         }
         private void LateUpdate()
@@ -57,6 +79,7 @@ namespace Tests.Characters
             base.Initialize(blackboard);
             blackboard.TryRegisterField(CharacterBlackboardFields.World, world);
             blackboard.TryRegisterField(CharacterBlackboardFields.GroundDetector, groundDetector);
+            blackboard.TryReadValueOrThrowException(CharacterBlackboardFields.Rigidbody, out _rbody);
         }
         public override void Dispose()
         {
