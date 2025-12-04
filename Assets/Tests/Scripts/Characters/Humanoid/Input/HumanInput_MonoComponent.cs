793b7df62eacdd8030f0fde8c306e5733c05171b
yanhaoran <yanhaoran_xvx@163.com>
2025-12-04 13:07:45 +0800

    refactor：改为继承HumanoidComponent，并补充逻辑

--- Assets/Tests/Scripts/Characters/Humanoid/Input/HumanInput_MonoComponent.cs
+++ Assets/Tests/Scripts/Characters/Humanoid/Input/HumanInput_MonoComponent.cs
@@ -1,14 +1,27 @@
-﻿using UnityEngine;
+﻿using Tests.Utilities.Blackboards;
+using UnityEngine;
 
 namespace Tests.Characters.Humanoid.Interaction.Input
 {
-    public class HumanInput_MonoComponent : MonoBehaviour
+    internal class HumanInput_MonoComponent : HumanoidComponent
     {
         [SerializeField]
         HumanInput _input;
         public static implicit operator HumanInput(HumanInput_MonoComponent mc)
         {
             return mc._input;
+        }
+        public override void Initialize(Blackboard blackboard)
+        {
+            base.Initialize(blackboard);
+            blackboard.TryRegisterField(CharacterBlackboardFields.Character_Input_Main_Base, _input.BaseInput);
+            blackboard.TryRegisterField(CharacterBlackboardFields.Character_Input_Main, _input);
+        }
+        public override void Dispose()
+        {
+            blackboard.TryUnregisterField(CharacterBlackboardFields.Character_Input_Main_Base, _input.BaseInput);
+            blackboard.TryUnregisterField(CharacterBlackboardFields.Character_Input_Main, _input);
+            base.Dispose();
         }
     }
 }
