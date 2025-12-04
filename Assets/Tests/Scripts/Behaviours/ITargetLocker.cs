087d5386f9cc734e191d2d1213fc570f1b67cc8e
yanhaoran <yanhaoran_xvx@163.com>
2025-12-04 12:32:14 +0800

    feat：新增ITargetLocker接口

---
+++ Assets/Tests/Scripts/Behaviours/ITargetLocker.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Tests.Interaction;
+using UnityEngine;
+
+namespace Tests.Behaviours
+{
+    internal interface ITargetLocker : ITargetLocker<ILockTarget>
+    {
+        Action<GameObject, GameObject> MainObjChangedAction { get; set; }
+
+    }
+}
