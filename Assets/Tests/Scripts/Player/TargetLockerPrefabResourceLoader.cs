049cc55e64a71bf17f88542c06e0bf201cdd6fc7
yanhaoran <yanhaoran_xvx@163.com>
2025-12-12 07:23:37 +0800

    refactor：删除无用的文件

--- Assets/Tests/Scripts/Player/TargetLockerPrefabResourceLoader.cs
+++
@@ -1,9 +0,0 @@
-﻿using Tests.Utilities.Assets_New;
-
-namespace Tests.Player
-{
-    internal class TargetLockerPrefabResourceLoader : PrefabResourceSingletonLoader<PlayerTargetLocker>
-    {
-
-    }
-}
