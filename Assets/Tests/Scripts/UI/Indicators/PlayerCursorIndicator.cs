145382dea42cb7a70a8d6996b9bfeb610a48e4a1
yanhaoran <yanhaoran_xvx@163.com>
2025-12-13 21:50:16 +0800

    feat：新增两个位置在中间的滑动条

--- Assets/Tests/Scripts/UI/Indicators/PlayerCursorIndicator.cs
+++ Assets/Tests/Scripts/UI/Indicators/PlayerCursorIndicator.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 
 namespace Tests.UI
 {
-    public class ActorCursorIndicator : CursorIndicator
+    public class PlayerCursorIndicator : CursorIndicator
     {
         [SerializeField]
         ProgressSlider _slider_lt;
@@ -13,11 +13,18 @@ namespace Tests.UI
         ProgressSlider _slider_rt;
         [SerializeField]
         ProgressSlider _slider_rb;
+        [SerializeField]
+        ProgressSlider _slider_lm;
+        [SerializeField]
+        ProgressSlider _slider_rm;
 
         public ProgressSlider Slider_lt { get => _slider_lt; }
         public ProgressSlider Slider_lb { get => _slider_lb; }
         public ProgressSlider Slider_rt { get => _slider_rt; }
         public ProgressSlider Slider_rb { get => _slider_rb; }
+        public ProgressSlider Slider_rm { get => _slider_rm; }
+        public ProgressSlider Slider_lm { get => _slider_lm; set => _slider_lm = value; }
+
         public override void Initialize(Blackboard blackboard)
         {
             base.Initialize(blackboard);
