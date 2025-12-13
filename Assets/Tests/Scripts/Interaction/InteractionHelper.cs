b185a4db147c3175ef0c649838593e024584ae05
yanhaoran <yanhaoran_xvx@163.com>
2025-12-13 21:55:17 +0800

    feat：新增InteractionHelper

---
+++ Assets/Tests/Scripts/Interaction/InteractionHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Tests.Interaction
+{
+    public class InteractionHelper
+    {
+        public static bool CheckFriendly(TeamMask mask, GameObject obj)
+        {
+            if (obj.TryGetComponent<ITeamMember>(out var memeber))
+                return memeber.CheckFriendlyBy(mask);
+            return false;
+        }
+    }
+}
