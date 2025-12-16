e0e6cb6cbbfba6af06a36d44e64f34bd6ea79b98
yanhaoran <yanhaoran_xvx@163.com>
2025-12-16 08:41:28 +0800

    feat：新增Player类

---
+++ Assets/Tests/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tests.Characters;
+using UnityEngine;
+
+namespace Tests.Players
+{
+    internal class Player : MonoBehaviour
+    {
+        CharacterComponent[] _components;
+        private void Awake()
+        {
+            DontDestroyOnLoad(this.gameObject);
+            _components = GetComponentsInChildren<CharacterComponent>();
+        }
+    }
+}
