421f085718310e63b310420b7b0f8f147a557e9c
yanhaoran <yanhaoran_xvx@163.com>
2025-12-17 21:12:57 +0800

    refactor：FilterCollecion独立为单独文件

---
+++ Assets/Tests/Scripts/Interaction/TargetLocker/FilterCollection.cs
@@ -0,0 +1,42 @@
+﻿using DG.Tweening;
+using System;
+using System.Collections.Generic;
+using Tests.Extensions;
+using UnityEngine;
+
+namespace Tests.Interaction
+{
+    public class FilterCollection
+    {
+        ICaughtItemFilter<GameObject>[] _filters;
+        public void AddFilter(ICaughtItemFilter<GameObject> filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException(nameof(filter));
+            if (_filters == null)
+                _filters = new ICaughtItemFilter<GameObject>[] { filter };
+            else
+                _filters = _filters.Append(filter);
+        }
+        public void RemoveFilter(ICaughtItemFilter<GameObject> filter)
+        {
+            if (filter == null)
+                throw new ArgumentNullException(nameof(filter));
+            if (_filters.Length == 1)
+                _filters = null;
+            else
+                _filters = _filters.Remove(filter);
+        }
+        public bool CanCatch(GameObject obj)
+        {
+            if (_filters == null)
+                return true;
+            foreach (var filter in _filters)
+            {
+                if (!filter.CanCatch(obj))
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- Assets/Tests/Scripts/Interaction/TargetLocker/PlayerTargetLocker.cs
+++ Assets/Tests/Scripts/Interaction/TargetLocker/PlayerTargetLocker.cs
@@ -119,40 +119,6 @@ namespace Tests.Interaction
             }
         }
 
-        internal class FilterCollection
-        {
-            ICaughtItemFilter<GameObject>[] _filters;
-            public void AddFilter(ICaughtItemFilter<GameObject> filter)
-            {
-                if (filter == null)
-                    throw new ArgumentNullException(nameof(filter));
-                if (_filters == null)
-                    _filters = new ICaughtItemFilter<GameObject>[] { filter };
-                else
-                    _filters = _filters.Append(filter);
-            }
-            public void RemoveFilter(ICaughtItemFilter<GameObject> filter)
-            {
-                if (filter == null)
-                    throw new ArgumentNullException(nameof(filter));
-                if (_filters.Length == 1)
-                    _filters = null;
-                else
-                    _filters = _filters.Remove(filter);
-            }
-            public bool CanCatch(GameObject obj)
-            {
-                if (_filters == null)
-                    return true;
-                foreach (var filter in _filters)
-                {
-                    if (!filter.CanCatch(obj))
-                        return false;
-                }
-                return true;
-            }
-        }
-
         #endregion
 
 
