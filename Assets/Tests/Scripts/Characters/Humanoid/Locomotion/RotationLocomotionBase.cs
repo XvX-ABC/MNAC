e1d47252cacf6838e42419f107730b76fa910342
yanhaoran <yanhaoran_xvx@163.com>
2025-12-18 12:46:41 +0800

    feat：新增RotationLocomotionBase抽象基类

---
+++ Assets/Tests/Scripts/Characters/Humanoid/Locomotion/RotationLocomotionBase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Tests.Interaction;
+using Tests.TPhysics.Locomotion;
+using Tests.Utilities.Blackboards;
+using Tests.Utilities.Composable;
+using LCore = Tests.TPhysics.Locomotion.LocomotionCore;
+
+namespace Tests.Characters.Humanoid.Locomotion
+{
+    public abstract class RotationLocomotionBase : ComponentBase
+    {
+        LCore _lcore;
+        IPositionTarget _target;
+        ITargetLocker<ILockTarget> _targetLocker;
+
+        protected RotationLocomotionBase(LCore locomotionCore)
+        {
+            _lcore = locomotionCore ?? throw new ArgumentNullException(nameof(locomotionCore));
+
+        }
+        ITargetLocker<ILockTarget> targetLocker
+        {
+            get => _targetLocker;
+            set
+            {
+                if (_targetLocker != null)
+                    _targetLocker.MainTargetChangedAction -= WhenTargetChange;
+                if (value != null)
+                {
+                    value.MainTargetChangedAction += WhenTargetChange;
+                    target = value.MainLockTarget;
+                }
+                _targetLocker = value;
+            }
+        }
+
+        internal virtual IPositionTarget target { get => _target; set => _target = value; }
+        public override bool Enabled
+        {
+            get => base.Enabled;
+            set
+            {
+                base.Enabled = value;
+                if (value)
+                    _lcore.EnableModule(rotationLocomotionModule);
+                else
+                    _lcore.DisableModule(rotationLocomotionModule);
+
+            }
+        }
+        protected abstract LocomotionModuleBase rotationLocomotionModule { get; }
+        public override void Initialize(Blackboard blackboard)
+        {
+            base.Initialize(blackboard);
+            _lcore.AddModule(rotationLocomotionModule);
+            if (!TryReadTargetLocker(blackboard))
+                blackboard.RegisterFieldChangeAction<ITargetLocker<ILockTarget>>(CharacterBlackboardFields.Character_Component_TargetLocker, WhenTargetLockerChange);
+            enabled = true;
+        }
+        public override void Dispose()
+        {
+            enabled = false;
+            blackboard.UnregisterFieldChangeAction<ITargetLocker<ILockTarget>>(CharacterBlackboardFields.Character_Component_TargetLocker, WhenTargetLockerChange);
+            _lcore.RemoveModule(rotationLocomotionModule);
+            base.Dispose();
+        }
+        void WhenTargetLockerChange(FieldEventType type, ITargetLocker<ILockTarget> oc, ITargetLocker<ILockTarget> nc)
+        {
+            if (type == FieldEventType.Reading)
+                return;
+            targetLocker = nc;
+        }
+        bool TryReadTargetLocker(Blackboard blackboard)
+        {
+            var r = blackboard.TryReadValue<IPlayerTargetLocker<ILockTarget>>(CharacterBlackboardFields.Character_Component_TargetLocker, out var targetLocker);
+            this.targetLocker = targetLocker;
+            return r;
+        }
+        protected void WhenTargetChange(ILockTarget _, ILockTarget target)
+        {
+            this.target = target;
+        }
+        public abstract void OnUpdate();
+    }
+}
